30c912b3c5c42a9f3d8c69d6908cd8c4af1de57e
EmptySpecter <specterknight2005@yandex.com>
2019-07-20 18:05:44 +0300

    code cleanup

--- src/main/java/ru/otus/spring/Main.java
+++ src/main/java/ru/otus/spring/Main.java
@@ -18,50 +18,7 @@ import java.util.Map;
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Main.class);
-
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Genre genre = new Genre(2, "Name");
-        Author author = new Author(3, "First", "Second");
-        Book book = new Book(4, "Book", 15, 10, genre, author);
-        Map<String, Object> map = objectMapper.convertValue(book, new TypeReference<Map<String, Object>>() {
-        });
-
-        System.out.println("\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        for (Map.Entry entry : map.entrySet()) {
-            Object value = entry.getValue();
-            if (value instanceof Map) {
-                for (Map.Entry subentry : ((Map<String, Object>) value).entrySet()) {
-                    System.out.print(subentry.getValue() + " ");
-                }
-            } else {
-                System.out.print(value + " ");
-            }
-        }
-        System.out.println();
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n");
-
-        AuthorRepository repository = context.getBean(AuthorRepository.class);
-        GenreRepository repository2 = context.getBean(GenreRepository.class);
-        BookRepository repository3 = context.getBean(BookRepository.class);
-        System.out.println(repository.getById(1));
-        System.out.println(repository.getAll());
-
-        System.out.println(repository2.getById(1));
-        System.out.println(repository2.getAll());
-
-        System.out.println(repository3.getAll());
-        System.out.println("//////////////////////////");
-        repository3.getById(20).ifPresent(System.out::println);
-        repository.getById(100).ifPresent(b -> System.out.println(repository3.getByAuthor(b)));
-        System.out.println(repository3.getByAuthor(new Author(200, "", "")));
-        System.out.println("-=======================================-");
-        repository3.save(new Book(1000, "Dopio", 10, 13, new Genre(1, "ds"), new Author(20, "ds", "dsd")));
-        System.out.println(repository3.getAll());
-        repository3.getById(200).ifPresent(System.out::println);
-        Console.main(args);
-
     }
 }
--- src/main/java/ru/otus/spring/shell/LibraryCommands.java
+++ src/main/java/ru/otus/spring/shell/LibraryCommands.java
@@ -25,6 +25,17 @@ import static ru.otus.spring.shell.TableHelper.getTableModel;
 @ShellComponent
 public class LibraryCommands {
 
+    private static final String ENTER_BOOK_TITLE = "Enter book title:\n";
+    private static final String TITLE_MUST_LESS_THAN_256_SYMBOLS = "Length of book title must less than 256 symbols!\n";
+    private static final String ENTER_NUMBER_OF_PAGES = "Enter number of pages:\n";
+    private static final String COUNT_MUST_BE_AN_INTEGER_NUMBER = "Page count must be an integer number!\n";
+    private static final String ENTER_NUMBER_OF_POINTS = "Enter number of points:\n";
+    private static final String POINTS_MUST_BE_AN_INTEGER_NUMBER = "Number of points must be an integer number!\n";
+    private static final String ENTER_AUTHOR_ID = "Enter author id:\n";
+    private static final String WRONG_AUTHOR_S_ID = "Wrong author's id!\n";
+    private static final String ENTER_GENRE_ID = "Enter genre id:\n";
+    private static final String WRONG_GENRE_S_ID = "Wrong genre's id!\n";
+
     private final Terminal terminal;
 
     private final AuthorRepository authorRepository;
@@ -41,34 +52,37 @@ public class LibraryCommands {
     @ShellMethod(value = "Display list of authors", key = {"al", "authors"})
     public void authorsList() {
         List<Author> authors = authorRepository.getAll();
-//todo if empty  list ???
-        TableModel model = getTableModel(authors);
-        TableBuilder tableBuilder = new TableBuilder(model);
-        tableBuilder.addInnerBorder(BorderStyle.fancy_light);
-        tableBuilder.addHeaderBorder(BorderStyle.fancy_double);
-        System.out.print(tableBuilder.build().render(80));
+        if (!authors.isEmpty()) {
+            TableModel model = getTableModel(authors);
+            TableBuilder tableBuilder = new TableBuilder(model);
+            tableBuilder.addInnerBorder(BorderStyle.fancy_light);
+            tableBuilder.addHeaderBorder(BorderStyle.fancy_double);
+            System.out.print(tableBuilder.build().render(80));
+        }
     }
 
     @ShellMethod(value = "Display list of genres", key = {"gl", "genres"})
     public void genresList() {
         List<Genre> genres = genreRepository.getAll();
-
-        TableModel model = getTableModel(genres);
-        TableBuilder tableBuilder = new TableBuilder(model);
-        tableBuilder.addInnerBorder(BorderStyle.fancy_light);
-        tableBuilder.addHeaderBorder(BorderStyle.fancy_double);
-        System.out.print(tableBuilder.build().render(80));
+        if (!genres.isEmpty()) {
+            TableModel model = getTableModel(genres);
+            TableBuilder tableBuilder = new TableBuilder(model);
+            tableBuilder.addInnerBorder(BorderStyle.fancy_light);
+            tableBuilder.addHeaderBorder(BorderStyle.fancy_double);
+            System.out.print(tableBuilder.build().render(80));
+        }
     }
 
     @ShellMethod(value = "Display list of books", key = {"bl", "books"})
     public void bookList() {
         List<Book> books = bookRepository.getAll();
-
-        TableModel model = getTableModel(books);
-        TableBuilder tableBuilder = new TableBuilder(model);
-        tableBuilder.addInnerBorder(BorderStyle.fancy_light);
-        tableBuilder.addHeaderBorder(BorderStyle.fancy_double);
-        System.out.print(tableBuilder.build().render(160));
+        if (!books.isEmpty()) {
+            TableModel model = getTableModel(books);
+            TableBuilder tableBuilder = new TableBuilder(model);
+            tableBuilder.addInnerBorder(BorderStyle.fancy_light);
+            tableBuilder.addHeaderBorder(BorderStyle.fancy_double);
+            System.out.print(tableBuilder.build().render(160));
+        }
     }
 
     @ShellMethod(value = "Add new book", key = {"ab", "add-book"})
@@ -78,38 +92,38 @@ public class LibraryCommands {
 
         String bookName;
         while (true) {
-            out.print("Enter book title:\n");
+            out.print(ENTER_BOOK_TITLE);
             bookName = in.nextLine();
             if (bookName.length() <= 255) break;
-            out.print("Length of book title must less than 256 symbols!\n");
+            out.print(TITLE_MUST_LESS_THAN_256_SYMBOLS);
         }
 
         int pagecount;
         while (true) {
-            out.print("Enter number of pages:\n");
+            out.print(ENTER_NUMBER_OF_PAGES);
             String tmp = in.nextLine();
             if (tmp.matches("[\\d]+")) {
                 pagecount = Integer.valueOf(tmp);
                 break;
             }
-            out.print("Page count must be an integer number!\n");
+            out.print(COUNT_MUST_BE_AN_INTEGER_NUMBER);
         }
 
 
         int points;
         while (true) {
-            out.print("Enter number of points:\n");
+            out.print(ENTER_NUMBER_OF_POINTS);
             String tmp = in.nextLine();
             if (tmp.matches("[\\d]+")) {
                 points = Integer.valueOf(tmp);
                 break;
             }
-            out.print("Number of points must be an integer number!\n");
+            out.print(POINTS_MUST_BE_AN_INTEGER_NUMBER);
         }
 
         Author author;
         while (true) {
-            out.print("Enter author id:\n");
+            out.print(ENTER_AUTHOR_ID);
             String tmp = in.nextLine();
             if (tmp.matches("[\\d]+")) {
                 int id = Integer.valueOf(tmp);
@@ -119,12 +133,12 @@ public class LibraryCommands {
                 } catch (NoSuchElementException e) {
                 }
             }
-            out.print("Wrong author's id!\n");
+            out.print(WRONG_AUTHOR_S_ID);
         }
 
         Genre genre;
         while (true) {
-            out.print("Enter genre id:\n");
+            out.print(ENTER_GENRE_ID);
             String tmp = in.nextLine();
             if (tmp.matches("[\\d]+")) {
                 int id = Integer.valueOf(tmp);
@@ -134,16 +148,16 @@ public class LibraryCommands {
                 } catch (NoSuchElementException e) {
                 }
             }
-            out.print("Wrong genre's id!\n");
+            out.print(WRONG_GENRE_S_ID);
         }
 
-        out.print("\n\nYou entered: \n");
+        out.print("\nYou entered: \n\n");
         out.print("Book title: " + bookName + "\n");
         out.print("Number of pages: " + pagecount + "\n");
         out.print("Number of points: " + points + "\n");
         out.print("Author name and surname: " + author.getName() + " " + author.getSurname() + "\n");
         out.print("Genre: " + genre.getName() + "\n");
-        out.print("\n\nWould you like to add this book to DB? [Y,N]:\n");
+        out.print("\nWould you like to add this book to DB? [Y,N]:\n");
         String confirmation = in.nextLine();
         if (confirmation.equals("Y")) {
             bookRepository.save(new Book(0, bookName, pagecount, points, genre, author));
