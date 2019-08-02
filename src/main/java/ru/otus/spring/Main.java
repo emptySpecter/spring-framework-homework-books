b80b78c576d3124f5933d99bf4789e4ac55c8fee
emptySpecter <specterknight2005@yandex.com>
2019-08-02 20:54:45 +0300

    just to save work

--- src/main/java/ru/otus/spring/Main.java
+++ src/main/java/ru/otus/spring/Main.java
@@ -2,6 +2,10 @@ package ru.otus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 public class Main {
@@ -10,5 +14,22 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, SPRING_SHELL_INTERACTIVE_ENABLED_TRUE);
+    }
+
+    //    @Bean
+//    public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator() {
+//        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+//        factory.setResources(new Resource[]{new ClassPathResource("genres.json")});
+//        return factory;
+//    }
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean getRespositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[]{
+                new ClassPathResource("authors.json"),
+                new ClassPathResource("genres.json"),
+                new ClassPathResource("books_with_author_and_genre.json")
+        });
+        return factory;
     }
 }
